8e3f4c2be8961bad9d5ae7f5c52a74457879f748
senadkurtisi <senad98kurtisi@gmail.com>
2021-11-23 10:25:32 +0100

    Added loading and preprocessing of image captions

---
+++ utils.py
@@ -0,0 +1,32 @@
+
+def load_descriptions(data):
+    """Processes the loaded image captions.
+
+    Image captions are saved in the following format:
+        image_id[SPACE]Caption
+    
+    Arguments:
+        data (str): Loaded image captions from .txt file
+    Returns:
+        image2desc (dict): Mapping from image id to all
+            captions of that image that occured in the dataset
+    """
+	image2desc = dict()
+	for sample in data.split('\n'):
+		tokens = sample.split()
+		if len(sample) < 2:
+            # Image has no description
+			continue
+		# First token is image id, remaining ones correspond to the caption
+		image_id, image_caption = tokens[0], tokens[1:]
+		# Extract only the filename from the image id
+		image_id = image_id.split('.')[0]
+		# Recreate the description
+		image_caption = " ".join(image_caption)
+
+		if image_id not in image2desc:
+			image2desc[image_id] = list()
+		# Save the description
+		image2desc[image_id].append(image_caption)
+
+	return image2desc
