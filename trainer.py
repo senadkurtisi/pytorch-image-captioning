47e9b208ff82adc38bcda640a75ee76639a2ac67
senadkurtisi <senad98kurtisi@gmail.com>
2021-11-30 17:43:18 +0100

    Finalized the training procedure

--- trainer.py
+++ trainer.py
@@ -1,3 +1,5 @@
+import time
+
 import numpy as np
 import torch
 import torch.nn as nn
@@ -6,11 +8,12 @@ import torchvision.models as models
 from nltk.translate.bleu_score import corpus_bleu
 
 from dataloader import Flickr8KDataset
-from decoder import CaptionDecoder
 from decoding_utils import greedy_decoding
+from decoder import CaptionDecoder
+from utils import save_checkpoint, log_gradient_norm
 
 
-def evaluate(subset, encoder, decoder, batch_size, max_len, device):
+def evaluate(subset, encoder, decoder, batch_size, max_len, device, bleu_weights):
     """Evaluates (BLEU score) caption generation model on a given subset.
 
     Arguments:
@@ -20,6 +23,8 @@ def evaluate(subset, encoder, decoder, batch_size, max_len, device):
         batch_size (int): Number of elements in each mini-batch
         max_len (int): Maximum length of generated captions
         device (torch.device): Device on which to port used tensors
+        bleu_weights (list of lists): Each element represents weights used for evaluating
+            BLEU-n score where n should be the length of the longest element in the list
     Returns:
         bleu (float): BLEU-4 score performance metric on the entire subset - corpus bleu
     """
@@ -33,6 +38,7 @@ def evaluate(subset, encoder, decoder, batch_size, max_len, device):
     references_total = []
     predictions_total = []
 
+    print("Evaluating model.")
     for x_img, y_caption in subset.inference_batch(batch_size):
         x_img = x_img.to(device)
         # Extract image features
@@ -44,9 +50,16 @@ def evaluate(subset, encoder, decoder, batch_size, max_len, device):
         # Get the caption prediction for each image in the mini-batch
         predictions = greedy_decoding(decoder, img_features, sos_id, eos_id, pad_id, idx2word, max_len, device)
         references_total += y_caption
+        # Remove EOS token
+        predictions = [caption.replace("<end>", "").strip() for caption in predictions]
         predictions_total += predictions
 
-    bleu = corpus_bleu(references_total, predictions_total)
+    # Calculate bleu score
+    bleu_1 = corpus_bleu(references_total, predictions_total, weights=list(bleu_weights["bleu-1"])) * 100
+    bleu_2 = corpus_bleu(references_total, predictions_total, weights=list(bleu_weights["bleu-2"])) * 100
+    bleu_3 = corpus_bleu(references_total, predictions_total, weights=list(bleu_weights["bleu-3"])) * 100
+    bleu_4 = corpus_bleu(references_total, predictions_total, weights=list(bleu_weights["bleu-4"])) * 100
+    bleu = [bleu_1, bleu_2, bleu_3, bleu_4]
     return bleu
 
 
@@ -65,6 +78,7 @@ def train(config, writer, device):
     train_hyperparams = {
         "batch_size": config["batch_size"]["train"],
         "shuffle": True,
+        "num_workers": 1,
         "drop_last": True
     }
 
@@ -92,7 +106,7 @@ def train(config, writer, device):
     learning_rate = train_config["learning_rate"]
 
     # Prepare the model optimizer
-    optimizer = torch.optim.Adam(
+    optimizer = torch.optim.AdamW(
         decoder.parameters(),
         lr=train_config["learning_rate"],
         weight_decay=train_config["l2_penalty"]
@@ -101,6 +115,7 @@ def train(config, writer, device):
     # Loss function
     loss_fcn = nn.CrossEntropyLoss()
 
+    start_time = time.strftime("%b-%d_%H-%M-%S")
     train_step = 0
     for epoch in range(train_config["num_of_epochs"]):
         print("Epoch:", epoch)
@@ -114,8 +129,6 @@ def train(config, writer, device):
             y, tgt_pos = y.to(device), tgt_pos.to(device)
             padding_mask = padding_mask.to(device)
 
-            optimizer.zero_grad()
-
             # Extract image features
             img_features = encoder(x_img)
             img_features = img_features.view(img_features.size(0), img_features.size(1), -1)
@@ -124,7 +137,7 @@ def train(config, writer, device):
 
             # Get the output of the decoder
             y_pred = decoder(x_words, img_features, padding_mask)
-            # Extract the prediction of the target token
+            # # Extract the prediction of the target token
             tgt_pos = tgt_pos.view(-1)
             y_pred = y_pred[torch.arange(y.size(0)), tgt_pos]
 
@@ -132,9 +145,15 @@ def train(config, writer, device):
             y = y.view(-1)
             loss = loss_fcn(y_pred, y)
 
+            # Update learning rate: lr scheduler from "Attention is All You Need"
+            learning_rate = (config["d_model"] ** (-0.5)) * min(train_step ** (-0.5), train_step * (train_config["warmup_steps"] ** (-1.5)))
+            for param_group in optimizer.param_groups:
+              param_group["lr"] = learning_rate
+
             # Update model weights
+            optimizer.zero_grad()
             loss.backward()
-            torch.nn.utils.clip_grad_norm_(decoder.parameters(), train_config["gradient_clipping"])
+            log_gradient_norm(decoder, writer, train_step, "Train")
             optimizer.step()
 
             writer.add_scalar("Train/Step-Loss", loss.item(), train_step)
@@ -144,9 +163,22 @@ def train(config, writer, device):
             encoder.eval()
             decoder.eval()
 
-            train_bleu = evaluate(train_set, encoder, decoder, config["batch_size"]["eval"], config["max_len"], device)
-            valid_bleu = evaluate(valid_set, encoder, decoder, config["batch_size"]["eval"], config["max_len"], device)
-            writer.add_scalar("Train/BLEU-4", train_bleu, epoch)
-            writer.add_scalar("Valid/BLEU-4", valid_bleu, epoch)
+            # Evaluate model performance on subsets
+            train_bleu = evaluate(train_set, encoder, decoder,
+                                  config["batch_size"]["eval"],
+                                  config["max_len"], device,
+                                  config["bleu_weights"])
+            valid_bleu = evaluate(valid_set, encoder, decoder,
+                                  config["batch_size"]["eval"],
+                                  config["max_len"], device,
+                                  config["bleu_weights"])
+
+            # Log the evaluated BLEU score
+            for i, t_b in enumerate(train_bleu):
+              writer.add_scalar(f"Train/BLEU-{i+1}", t_b, epoch)
+            for i, v_b in enumerate(valid_bleu):
+              writer.add_scalar(f"Valid/BLEU-{i+1}", v_b, epoch)
 
             decoder.train()
+        
+        save_checkpoint(decoder, start_time, epoch)
